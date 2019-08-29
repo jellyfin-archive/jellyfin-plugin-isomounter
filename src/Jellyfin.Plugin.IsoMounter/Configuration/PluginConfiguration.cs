1d72d22ffa0f79941379ea8631dabcb8d9fd7337
Bond_009 <bond.009@outlook.com>
2019-08-29 23:06:48 +0200

    First

---
+++ src/Jellyfin.Plugin.IsoMounter/Configuration/PluginConfiguration.cs
@@ -0,0 +1,11 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.IsoMounter.Configuration
+{
+    /// <summary>
+    /// Class PluginConfiguration.
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+    }
+}
